b6cab98850a70d200d5464fa0691189c4e51fef6
Mohammadreza Mousaviniya <mohammadrezamousaviniya@gmail.com>
2021-10-12 13:40:22 +0330

    Add project files.

---
+++ EroyaLearnLotteryV00/LotteryPage.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace EroyaLearnLotteryV00
+{
+    partial class LotteryPage
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LotteryPage));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.lb_Mmbrs = new System.Windows.Forms.ListBox();
+            this.lbl_mmbrsCount = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_WinnerCounts = new System.Windows.Forms.TextBox();
+            this.LLbl_redirectToEroya = new System.Windows.Forms.LinkLabel();
+            this.btn_Search = new System.Windows.Forms.Button();
+            this.btn_dimodal = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 75F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1440, 868);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.lb_Mmbrs, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.lbl_mmbrsCount, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.700696F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 91.2993F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1074, 862);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // lb_Mmbrs
+            // 
+            this.lb_Mmbrs.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lb_Mmbrs.FormattingEnabled = true;
+            this.lb_Mmbrs.ItemHeight = 41;
+            this.lb_Mmbrs.Location = new System.Drawing.Point(3, 78);
+            this.lb_Mmbrs.Name = "lb_Mmbrs";
+            this.lb_Mmbrs.Size = new System.Drawing.Size(1068, 781);
+            this.lb_Mmbrs.TabIndex = 0;
+            // 
+            // lbl_mmbrsCount
+            // 
+            this.lbl_mmbrsCount.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbl_mmbrsCount.ForeColor = System.Drawing.Color.White;
+            this.lbl_mmbrsCount.Location = new System.Drawing.Point(3, 0);
+            this.lbl_mmbrsCount.Name = "lbl_mmbrsCount";
+            this.lbl_mmbrsCount.Size = new System.Drawing.Size(1068, 75);
+            this.lbl_mmbrsCount.TabIndex = 1;
+            this.lbl_mmbrsCount.Text = "Members Count : 0";
+            this.lbl_mmbrsCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tableLayoutPanel3);
+            this.panel1.Controls.Add(this.LLbl_redirectToEroya);
+            this.panel1.Controls.Add(this.btn_Search);
+            this.panel1.Controls.Add(this.btn_dimodal);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(1083, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(354, 862);
+            this.panel1.TabIndex = 1;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.txt_WinnerCounts, 0, 1);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(0, 200);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(354, 606);
+            this.tableLayoutPanel3.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(348, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of Winners :";
+            // 
+            // txt_WinnerCounts
+            // 
+            this.txt_WinnerCounts.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txt_WinnerCounts.Location = new System.Drawing.Point(3, 53);
+            this.txt_WinnerCounts.Name = "txt_WinnerCounts";
+            this.txt_WinnerCounts.Size = new System.Drawing.Size(348, 47);
+            this.txt_WinnerCounts.TabIndex = 1;
+            this.txt_WinnerCounts.TextChanged += new System.EventHandler(this.txt_WinnerCounts_TextChanged);
+            // 
+            // LLbl_redirectToEroya
+            // 
+            this.LLbl_redirectToEroya.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.LLbl_redirectToEroya.LinkColor = System.Drawing.Color.White;
+            this.LLbl_redirectToEroya.Location = new System.Drawing.Point(0, 806);
+            this.LLbl_redirectToEroya.Name = "LLbl_redirectToEroya";
+            this.LLbl_redirectToEroya.Size = new System.Drawing.Size(354, 56);
+            this.LLbl_redirectToEroya.TabIndex = 3;
+            this.LLbl_redirectToEroya.TabStop = true;
+            this.LLbl_redirectToEroya.Text = "Eroya In Tech Lottery V00";
+            this.LLbl_redirectToEroya.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.LLbl_redirectToEroya.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LLbl_redirectToEroya_LinkClicked);
+            // 
+            // btn_Search
+            // 
+            this.btn_Search.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_Search.Location = new System.Drawing.Point(0, 100);
+            this.btn_Search.Name = "btn_Search";
+            this.btn_Search.Size = new System.Drawing.Size(354, 100);
+            this.btn_Search.TabIndex = 1;
+            this.btn_Search.Text = "Find The Winner";
+            this.btn_Search.UseVisualStyleBackColor = true;
+            this.btn_Search.Click += new System.EventHandler(this.btn_Search_Click);
+            // 
+            // btn_dimodal
+            // 
+            this.btn_dimodal.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_dimodal.Location = new System.Drawing.Point(0, 0);
+            this.btn_dimodal.Name = "btn_dimodal";
+            this.btn_dimodal.Size = new System.Drawing.Size(354, 100);
+            this.btn_dimodal.TabIndex = 0;
+            this.btn_dimodal.Text = "Members Excel File";
+            this.btn_dimodal.UseVisualStyleBackColor = true;
+            this.btn_dimodal.Click += new System.EventHandler(this.btn_dimodal_Click);
+            // 
+            // LotteryPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::EroyaLearnLotteryV00.Properties.Resources.BG;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1440, 868);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LotteryPage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Eroya Learn Lottery";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.ListBox lb_Mmbrs;
+        private System.Windows.Forms.Label lbl_mmbrsCount;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_WinnerCounts;
+        private System.Windows.Forms.LinkLabel LLbl_redirectToEroya;
+        private System.Windows.Forms.Button btn_Search;
+        private System.Windows.Forms.Button btn_dimodal;
+    }
+}
+
---
+++ EroyaLearnLotteryV00/LotteryPage.cs
@@ -0,0 +1,135 @@
+﻿using Syncfusion.XlsIO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EroyaLearnLotteryV00
+{
+    public partial class LotteryPage : Form
+    {
+        public LotteryPage()
+        {
+            InitializeComponent();
+            btn_Search.Enabled = false;
+            txt_WinnerCounts.Text = "1";
+        }
+
+        #region ImportantVar
+        string FilePath { get; set; }
+        List<string> Members { get; set; }
+
+        #endregion
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LLbl_redirectToEroya_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // TNX StackOverFlow
+            var ps = new ProcessStartInfo("https://www.eroyaintech.com/")
+            {
+                UseShellExecute = true,
+                Verb = "open"
+            };
+            Process.Start(ps);
+
+        }
+
+        private void btn_dimodal_Click(object sender, EventArgs e)
+        {
+            //open file Dialogue
+            using (OpenFileDialog OFD = new OpenFileDialog())
+            {
+                OFD.InitialDirectory = @"D:\projects";
+                OFD.Filter = "Excel File|*.xls;*.xlsx;*.xlsm";
+                OFD.RestoreDirectory = true;
+                OFD.FilterIndex = 2;
+                if (OFD.ShowDialog() == DialogResult.OK)
+                {
+                    FilePath = OFD.FileName;
+                    btn_Search.Enabled = true;
+                    ExcelEngine excelEngine = new ExcelEngine();
+                    using (var stream = File.Open(FilePath, FileMode.Open, FileAccess.Read))
+                    {
+                        var excelfile = excelEngine.Excel.Workbooks.Open(stream);
+                        var firstSheet = excelfile.Worksheets["Sheet1"];
+                        lbl_mmbrsCount.Text = $"Members Count : {firstSheet.Rows.Count()}";
+                        Members = new List<string>();
+                        foreach (var item in firstSheet.Rows)
+                        {
+                            Members.Add(item.Columns[0].Value);
+                            lb_Mmbrs.Items.Add(item.Columns[0].Value);
+                        }
+                    }
+
+
+
+                }
+            }
+        }
+
+        private void btn_Search_Click(object sender, EventArgs e)
+        {
+            int membercount = Members.Count;
+            int winnerCount = 0;
+            List<int> winnerIndex = new List<int>();
+            if (!string.IsNullOrEmpty(txt_WinnerCounts.Text))
+            {
+                winnerCount = Convert.ToInt32(txt_WinnerCounts.Text);
+                for (int i = 0; i < winnerCount; i++)
+                {
+                    Random Rand = new Random();
+                    winnerIndex.Add(Rand.Next(0, membercount - 1));
+                }
+                string result = "";
+                for (int i = 0; i < winnerIndex.Count; i++)
+                {
+                    result += $"Winner no.{i + 1} : InstagramID - {Members[winnerIndex[i]]}\n";
+                }
+                MessageBox.Show(result,"We Have Winners !",  MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+
+
+            }
+            else
+            {
+                MessageBox.Show("Enter Winners Count","Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void txt_WinnerCounts_TextChanged(object sender, EventArgs e)
+        {
+            int Flag = 0;
+            try
+            {
+                if (!Int32.TryParse(txt_WinnerCounts.Text, out Flag))
+                {
+                    
+                    txt_WinnerCounts.Text = "1";
+                }
+                else
+                {
+                    if (Flag <= 0) 
+                    {
+                        txt_WinnerCounts.Text = "1";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                txt_WinnerCounts.Text = "1";
+            }
+        }
+    }
+}
